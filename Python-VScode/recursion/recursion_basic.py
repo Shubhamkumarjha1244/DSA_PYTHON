90576ab28bd782ba2b168b92e7a84a66004d4110
shubham kumar jha <jha.kumar.shubham1244@gmail.com>
2022-03-30 11:25:07 +0530

    practice

--- Python-VScode/recursion/recursion_basic.py
+++ Python-VScode/recursion/recursion_basic.py
@@ -67,20 +67,21 @@ def unique_array(arr):                              #all unique element in array
 
 
 
-def binary_search_mine(arr,key):       #array is defalut sorted
+def binary_search_mine(arr,key):       #array is deflaut sorted
     if len(arr)==0:
         return False
     mid=(int(len(arr)/2))
     if arr[mid] == key:
         return True
     if key > arr[mid]:
-        return binary_search_mine(arr[mid+1:],key)
+       return binary_search_mine(arr[mid+1:],key)
     else:
+
         return binary_search_mine(arr[:mid],key)
 
     
     
-def binary_search(arr,key,startindex,endindex):       #array is defalut sorted
+def binary_search(arr,key,startindex,endindex):       #array must be is deflaut sorted
     if startindex > endindex:
         return -1
     mid=(startindex+endindex)//2
@@ -90,6 +91,52 @@ def binary_search(arr,key,startindex,endindex):       #array is defalut sorted
         return binary_search(arr,key,mid+1,endindex)
     else:
         return binary_search(arr,key,startindex,mid-1)
+def merge(arr1,arr2,arr):
+        i=0
+        j=0
+        k=0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] >arr2[j] :
+                arr[k]=arr2[j]
+                j+=1
+                k+=1
+            else:
+                arr[k]=arr1[i]
+                i+=1
+                k+=1
+        while  i < len(arr1):
+            arr[k]=arr1[i]
+            i+=1
+            k+=1
+        
+        while j < len(arr2):
+            arr[k]=arr2[j]
+            j+=1
+            k+=1
+        return arr
+
+def merge_sort(arr):
+    if len(arr)==1 or len(arr)==0:
+        return
+    mid=len(arr)//2 #to find middle using floor division
+    a1=arr[:mid]
+    a2=arr[mid:]
+    merge_sort(a1)
+    merge_sort(a2)
+    return merge(a1,a2,arr)
+
+def binary_search_practice(arr,key,st,en):
+    if st > en:
+        return -1
+    mid =(st+en)//2
+    if arr[mid] == key :
+        return mid
+    elif arr[mid]>key:
+        return binary_search_practice(arr,key,st,mid-1)
+    else:
+        return binary_search_practice(arr,key,mid+1,en)
+    
+
 
 
 
@@ -112,6 +159,9 @@ print(str,'\n',replace_pi(str))
 arr=[1,1,2,3,4,4,4,5,6,6]
 print(arr,'---',unique_array(arr))
 arr=[1,2,3,4,5,6,7,8,9,10,11,12]
-key=16
+key=6
 print(arr,'---',binary_search_mine(arr,key))
-print(arr,'---',binary_search(arr,key,0,11))
+print(arr,'---practice---',binary_search_practice(arr,1,0,11))
+arr1=[2,1,3,4,5,6,7,8,9,10,12,11]
+arr2=merge_sort(arr1)
+print('unsorted array---',arr1,'sorted array---',arr2)
