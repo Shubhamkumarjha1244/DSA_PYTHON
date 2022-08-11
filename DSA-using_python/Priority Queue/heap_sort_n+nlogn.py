d43d571058366467e47f432a9753c2a21ec55872
shubham kumar jha <jha.kumar.shubham1244@gmail.com>
2022-08-11 09:57:40 +0530

    heap sort O(logn) and O(1)

---
+++ DSA-using_python/Priority Queue/heap_sort_n+nlogn.py
@@ -0,0 +1,53 @@
+def heap_sort_n_nlogn(input):
+    make_heap(input)
+    take_from_heap(input)
+    return input
+
+
+def down_heapify_from(input,i):
+    leng=len(input)
+    while (2*i+1)<leng and (2*i+2)<leng:
+        left=(2*i+1)
+        if (2*i+2)>=leng:right_val=1e12
+        else:right_val=input[2*i+2]
+        mini=min(input[left],right_val,input[i])
+        if mini==input[i]:break
+        elif mini==input[left]:
+            input[i],input[left]=input[left],input[i]
+            i=left
+        elif mini==right_val:
+            input[i],input[2*i+2]=input[2*i+2],input[i]
+            i=2*i+2
+
+def down_heapyfy_till(input,end):
+    i=0
+    while (2*i+1)<end or (2*i+2)<end:
+        left=2*i+1
+        if (2*i+2)>=end:right_val=2e10
+        else:right_val=input[2*i+2]
+        mini=min(input[left],input[i],right_val)
+        if mini==input[i]:break
+        elif mini==input[left]:
+            input[i],input[left]=input[left],input[i]
+            i=left
+        elif mini==right_val:
+            input[i],input[2*i+2]=input[2*i+2],input[i]
+            i=(2*i+2)
+
+def make_heap(input):
+    non_leaf=len(input)//2
+    for i in range(non_leaf-1,-1,-1):
+        down_heapify_from(input,i)
+    return input
+
+def take_from_heap(input):
+    leng=len(input)
+    for i in range(len(input)):
+        input[0],input[leng-1-i]=input[leng-1-i],input[0]
+        down_heapyfy_till(input,leng-1-i)
+    return input
+
+
+
+input=[4,6,7,1,10000,-12332,53,5,98,98]
+print(heap_sort_n_nlogn(input))
---
+++ DSA-using_python/Priority Queue/heap_sort_no_new_space.py
@@ -0,0 +1,47 @@
+
+def heap_insertion(input):
+    for i in range(len(input)):
+        coculate_up(input,i)
+    return input
+def coculate_up(arr,i):
+    while i>0:
+        parent=(i-1)//2
+        if arr[parent]>arr[i]:
+            arr[parent],arr[i]=arr[i],arr[parent]
+            i=parent
+        else:break
+    
+
+def coculate_down(arr,end):
+    i=0
+    while (2*i)+1<end or (2*i)+2<end:
+        left=(2*i)+1
+        right=(2*i)+2
+        if right >= end:right_val=2e6    
+        else:right_val=arr[right]
+        mini=min(arr[i],arr[left],right_val)
+        if mini==arr[i]:break
+        elif mini==arr[left]:
+            arr[left],arr[i]=arr[i],arr[left]
+            i=left
+        elif mini==right_val:
+            arr[right],arr[i]=arr[i],arr[right]
+            i=right
+
+def heap_deletion(input):
+    for i in range(len(input)):
+        input[0],input[len(input)-1-i]=input[len(input)-1-i],input[0]
+        coculate_down(input,len(input)-1-i) 
+    return input
+
+def heap_sort(input):
+    input=heap_insertion(input)
+    print('hi',input)
+    input=heap_deletion(input)
+    return input
+
+
+
+
+input=[4,6,7,1,10000,53,5,98,98]
+print(heap_sort(input))
