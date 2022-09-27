7364a53c6d0b3c275dec49934c9ccafd7a72b952
shubham kumar jha <jha.kumar.shubham1244@gmail.com>
2022-09-27 16:26:27 +0530

    check_bi_partile_graph using dfs

--- DSA-using_python/SDE graphs/check_bi_partile_graph_using_dfs.py
+++ DSA-using_python/SDE graphs/check_bi_partile_graph_using_dfs.py
@@ -14,3 +14,36 @@ class graph:
 
 
 
+
+
+
+def dfs_helper(st,adj,total_vertex,color):
+    for i in range(total_vertex):
+        if adj[st][i]==1:
+            if color[i]==-1:
+                color[i]=1-color[st]
+                if dfs_helper(i,adj,total_vertex,color)==False:return False
+            else:
+                if color[i]==color[st]:return False
+    return True
+
+def checkBipartileGraphUsingdfs(adj,total_vertex):
+    color=[-1 for i in range(total_vertex)]
+    for i in range(total_vertex):
+        if color[i]==-1:
+            color[i]=0
+            if dfs_helper(i,adj,total_vertex,color)==False:return False
+    return True
+
+
+
+g=graph(4)
+g.addEdge(0,1)
+g.addEdge(1,2)    
+g.addEdge(2,3)
+g.addEdge(0,2)
+g.addEdge(0,3)
+
+print(checkBipartileGraphUsingdfs(g.adjMatrix,g.vertex))
+
+    
