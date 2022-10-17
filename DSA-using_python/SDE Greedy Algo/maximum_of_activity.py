fcd69d5abf3fb96a29eb0820bbaec9dcd55f447b
shubham kumar jha <jha.kumar.shubham1244@gmail.com>
2022-10-17 17:18:01 +0530

    Greedy algo Questions Maximum of activities

---
+++ DSA-using_python/SDE Greedy Algo/maximum_of_activity.py
@@ -0,0 +1,18 @@
+def max_no_meeting(n,start,end):
+    meeting=[]
+    for i in range(n):
+        meeting.append((start[i],end[i]))
+    meeting.sort(key=lambda a:a[1])
+    end=meeting[0][1]
+    ans=1
+    for meet in meeting[1:]:
+        if meet[0]>=end:
+            ans+=1
+            end=meet[1]
+    return ans
+
+
+n=4
+act_start=[1,6,2,4]
+act_end=[2,7,5,8]
+print(max_no_meeting(n,act_start,act_end))
